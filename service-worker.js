1184f0c624d4e9de44aa1932bbf5a4a5319df870
tkarlsson1 <timmykarlsson@gmail.com>
2025-05-27 19:40:13 +0200

    446

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("app-cache").then(cache => {
+      return cache.addAll(["/", "/index.html"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
