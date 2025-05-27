1046ebbe23e8c90c84d7b7a5aad6334a0778b41b
tkarlsson1 <timmykarlsson@gmail.com>
2025-05-28 00:29:05 +0200

    test

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,6 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("app-cache").then(cache => {
-      return cache.addAll(["/", "/index.html"]);
-    })
-  );
-});
-
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
   );
 });
