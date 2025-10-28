400ac82d6a00ded6975ffbb02fe90960f1b0ac3e
tkarlsson1 <timmykarlsson@gmail.com>
2025-10-29 00:44:41 +0100

    577

--- test/assets/js/assets/js/spotify.js
+++
@@ -1,5 +0,0 @@
-
-if (!window.ns) window.ns = {};
-
-// Extracted from legacy script.js into spotify.js
-console.log("Uppdaterar customYear för playlist:", currentPlaylistName, "spotifyID:", currentSpotifyID);
